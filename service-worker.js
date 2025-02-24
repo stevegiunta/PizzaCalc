1efb94f1d78c2a4f7a9888247571849cbc319805
stevegiunta <steve.4gs@gmail.com>
2025-02-24 16:53:05 -0500

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,22 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('pizza-calc-v1').then((cache) => {
+    caches.open('pizza-calc-v2').then((cache) => { // Changed to v2
       return cache.addAll(['pizza_calc.html', 'manifest.json']);
     })
   );
+  self.skipWaiting(); // Force new version
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== 'pizza-calc-v2')
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim(); // Take control immediately
 });
 
 self.addEventListener('fetch', (event) => {
